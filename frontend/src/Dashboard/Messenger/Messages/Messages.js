adfc696f369932c41e4b34e3bcfd188dc902394c
dathdka <dathdka@gmail.com>
2022-08-27 17:02:49 +0700

    prepare ui to send messages

--- frontend/src/Dashboard/Messenger/Messages/Messages.js
+++ frontend/src/Dashboard/Messenger/Messages/Messages.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React from "react";
 import { styled } from "@mui/system";
 import MessagesHeader from "./MessagesHeader";
 import { connect } from "react-redux";
 import DUMMY_MESSAGES from "./DUMMY_MESSAGES";
+import Message from "./Message";
 
 const MainContainer = styled("div")({
-    height: "calc(100% - 60px)",
-    overflow: "auto",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  });
-
+  height: "calc(100% - 60px)",
+  overflow: "auto",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+});
 
 const Messages = ({ chosenChatDetails, messages }) => {
-    return (
-        <MainContainer>
-            <MessagesHeader name = {chosenChatDetails?.name}/>
-        </MainContainer>
-    );
+  return (
+    <MainContainer>
+      <MessagesHeader name={chosenChatDetails?.name} />
+      {DUMMY_MESSAGES.map((message, index) => {
+        return (
+          <Message
+            key={message._id}
+            content={message.content}
+            username={message.author.username}
+            sameAuthor={message.sameAuthor}
+            date={message.date}
+            sameDay={message.sameDay}
+          />
+        );
+      })}
+    </MainContainer>
+  );
 };
 
 const mapStoreStateToProps = ({ chat }) => {
-    return {
-      ...chat,
-    };
+  return {
+    ...chat,
   };
+};
 export default connect(mapStoreStateToProps)(Messages);
